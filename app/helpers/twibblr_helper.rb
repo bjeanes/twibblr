ee849dd3071aa56270d92c4a24cd08d6deb3ea55
Bodaniel Jeanes <me@bjeanes.com>
2009-02-13 19:20:47 +1000

    Loading entire site including theme, posts, tags, and post count from plugin in a vanilla app with NO CODE MODIFICATIONS other than datbase.yml

--- app/helpers/twibblr_helper.rb
+++ app/helpers/twibblr_helper.rb
@@ -1,9 +1,34 @@
 module TwibblrHelper
-  def render_posts
+  def render_posts(*options, &block)
+    # if they are fetching posts in the controller
+    # then awesome(!) otherwise get it for them
+    posts = @posts || Post.all(*options)
+
     if block_given?
-      @posts.each { |post| yield post.title, post.body, post.created_at }
+      posts.each do |post|
+        render_post(post, &block)
+      end
     else
-      render :partial => @posts
+      render :partial => posts
+    end
+  end
+
+  def render_tags(*options)
+    raise "No Block Given" unless block_given?
+    (@tags || Tag.all(*options)).each {|tag| yield(tag)}
+  end
+
+  private
+
+  def render_post(post, &block)
+    raise "No block given" unless block_given?
+    case block.arity
+    when 1 then yield(post)
+    when 2 then yield(post.title, post.body)
+    when 3 then yield(post.title, post.body, post.created_at)
+    when 4 then yield(post.title, post.body, post.created_at, post.tags)
+    when 5 then yield(post.title, post.body, post.created_at, post.tags, post.comments)
+    else raise "Parameter Error"
     end
   end
 end
